99409eaa6f84f2f6e95766e0273ec3d74c8a6e1d
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-05-01 23:53:19 +0300

    Update test

--- github-pullrequest-plugin/src/test/java/org/jenkinsci/plugins/github_integration/its/MatrixProjectITest.java
+++ github-pullrequest-plugin/src/test/java/org/jenkinsci/plugins/github_integration/its/MatrixProjectITest.java
@@ -6,12 +6,18 @@ import hudson.matrix.MatrixProject;
 import hudson.matrix.TextAxis;
 import hudson.tasks.Shell;
 import org.jenkinsci.plugins.github.pullrequest.GitHubPRMessage;
+import org.jenkinsci.plugins.github.pullrequest.GitHubPRTrigger;
+import org.jenkinsci.plugins.github.pullrequest.GitHubPRTriggerMode;
 import org.jenkinsci.plugins.github.pullrequest.builders.GitHubPRStatusBuilder;
+import org.jenkinsci.plugins.github.pullrequest.events.GitHubPREvent;
+import org.jenkinsci.plugins.github.pullrequest.events.impl.GitHubPRCommitEvent;
+import org.jenkinsci.plugins.github.pullrequest.events.impl.GitHubPROpenEvent;
 import org.jenkinsci.plugins.github.pullrequest.publishers.impl.GitHubPRBuildStatusPublisher;
 import org.jenkinsci.plugins.github.pullrequest.publishers.impl.GitHubPRCommentPublisher;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.jenkinsci.plugins.github_integration.junit.GHRule.getPreconfiguredProperty;
 import static org.jenkinsci.plugins.github_integration.junit.GHRule.getPreconfiguredTrigger;
@@ -25,7 +31,20 @@ public class MatrixProjectITest extends AbstractPRTest {
     public void testChildStatuses() throws Exception {
         final MatrixProject matrixProject = j.jenkins.createProject(MatrixProject.class, "matrix-project");
 
-        matrixProject.addTrigger(getPreconfiguredTrigger());
+        matrixProject.setAxes(
+                new AxisList(
+                        new TextAxis("first_axis", "first_value1", "first_value2"),
+                        new TextAxis("second_axis", "sec_value1", "sec_value2")
+                )
+        );
+
+        final ArrayList<GitHubPREvent> gitHubPREvents = new ArrayList<>();
+        gitHubPREvents.add(new GitHubPROpenEvent());
+        gitHubPREvents.add(new GitHubPRCommitEvent());
+
+        final GitHubPRTrigger gitHubPRTrigger = new GitHubPRTrigger("", GitHubPRTriggerMode.CRON, gitHubPREvents);
+
+        matrixProject.addTrigger(gitHubPRTrigger);
         matrixProject.addProperty(getPreconfiguredProperty(ghRule.getGhRepo()));
 
         matrixProject.getBuildersList().add(new GitHubPRStatusBuilder());
@@ -34,17 +53,8 @@ public class MatrixProjectITest extends AbstractPRTest {
         matrixProject.getPublishersList().add(new GitHubPRBuildStatusPublisher());
         matrixProject.getPublishersList().add(new GitHubPRCommentPublisher(new GitHubPRMessage("Comment"), null, null));
 
-
-        matrixProject.setAxes(
-                new AxisList(
-                        new TextAxis("first_axis", "first_value1", "first_value2"),
-                        new TextAxis("second_axis", "sec_value1", "sec_value2")
-                )
-        );
-
         matrixProject.save();
 
         super.basicTest(matrixProject);
-
     }
 }
